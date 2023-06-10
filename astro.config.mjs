a1aa14d2e20b743a63111c2400557c4d9310fc55
Noob Science <noobscience123@gmail.com>
2023-06-10 16:31:14 +0530

    Small test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
