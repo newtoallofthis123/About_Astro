9444486f8fe1d9f882c7aa11abbdd3d18c2fc7f2
Noob Science <noobscience123@gmail.com>
2023-06-14 00:01:15 +0530

    Migrate to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
   markdown: {
     gfm: true,
     shikiConfig: {
       theme: 'dracula',
       langs: ['javascript', 'html', 'css', 'typescript', 'tsx', 'jsx', 'json', 'markdown', 'mdx', 'bash', 'shell', 'sh', 'yaml', 'yml', 'graphql', 'md', 'mdx'],
-      wrap: true,
+      wrap: true
     },
     syntaxHighlight: 'shiki'
   }
