7e344406be2b5f72a6e917190861e49a55f710ae
Noob Science <noobscience123@gmail.com>
2023-06-21 00:11:40 +0530

    Add Sup Page

--- src/components/updates/update.tsx
+++ src/components/updates/update.tsx
@@ -9,6 +9,7 @@ export default function Update({ hash }: Props) {
     const [latest, setLatest] = useState({
         name: '',
         content: '',
+        author: '',
         date: '',
         hash: '',
     });
@@ -35,11 +36,11 @@ export default function Update({ hash }: Props) {
                 <h1 className="text-center p-4">Loading...</h1>
             )}
             {latest.name != '' && (
-                <div className=" m-2 md:mx-10 mt-0 p-2 md:p-10">
+                <div className="m-2 md:mx-20 mt-0 p-2 md:p-10">
                     <h1 className="md:text-4xl text-3xl text-center py-4 pt-0 font-bold">
                         {latest.name}
                     </h1>
-                    <h2 className="md:text-2xl font-light text-center text-xl">{latest.date}</h2>
+                    <h2 className="md:text-2xl font-light text-center text-xl">By {latest.author} on {latest.date}</h2>
                     <div
                         className="text-xl md:pt-8 p-4 md:p-0 leading-10"
                         dangerouslySetInnerHTML={{
